3a94ee73c4ba7b887d6d87b2521e0099310012f3
Alan Morgan <amorgan@nextlabs.com>
2015-07-01 15:12:23 -0700

    Added print and rotate methods (which work).

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,32 @@ impl Polyomino {
     pub fn bottom_left(&self) -> Point {
         self.points.iter().fold(self.points[0], |a, ref p| Point { x: cmp::min(a.x, p.x), y: cmp::min(a.y, p.y) })
     }
+
+    pub fn show(&self) {
+        // Inefficient, but it hardly matters
+        let Point { x: width, y: height } = self.top_right();
+        for y in 0..height+1 {
+            for x in 0..width+1 {
+                if let Some(_p) = self.points.iter().find(|ref p| p.x == x && p.y == (height-y)) {
+                    print!("X");
+                } else {
+                    print!(" ");
+                }
+            }
+            println!("");
+        }
+    }
+
+    pub fn rotatecw(&self) -> Polyomino {
+        let height = self.top_right().y;
+
+        let mut new_points = Vec::new();
+        
+        for p in self.points.iter().map(|p| Point {x: height-p.y, y: p.x}) {
+            new_points.push(p);
+        }
+        Polyomino{ points: new_points }
+    }
 }
 
 fn main() {
@@ -27,5 +53,11 @@ fn main() {
     println!("p = {:?}", p);
     println!("top right   = {:?}", p.top_right());
     println!("bottom left = {:?}", p.bottom_left());
+
+    p.show();
+    println!("");
+    p.rotatecw().show();
+    println!("");
+    p.rotatecw().rotatecw().show();
 }
       
