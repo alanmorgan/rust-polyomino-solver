96ca905ddcb3a30361bd4902fc8cbce021c5e857
Alan Morgan <amorgan@nextlabs.com>
2015-06-30 17:42:24 -0700

    Functions to find the top left and bottom right of a polyomino.
    Point must be Clone and Copy, because the fold takes an
    actual value, not a reference.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::cmp;
+
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: i32,
     y: i32
@@ -9,9 +11,21 @@ struct Polyomino {
     points: Vec<Point>
 }
 
+impl Polyomino {
+    pub fn top_right(&self) -> Point {
+        self.points.iter().fold(self.points[0], |a, ref p| Point { x: cmp::max(a.x, p.x), y: cmp::max(a.y, p.y) })
+    }
+    
+    pub fn bottom_left(&self) -> Point {
+        self.points.iter().fold(self.points[0], |a, ref p| Point { x: cmp::min(a.x, p.x), y: cmp::min(a.y, p.y) })
+    }
+}
+
 fn main() {
     let p = Polyomino{points: vec![Point{x:0,y:1}, Point{x:1,y:0}, Point{x:1,y:1}, Point{x:1,y:2}, Point{x:2,y:2}]};
 
     println!("p = {:?}", p);
+    println!("top right   = {:?}", p.top_right());
+    println!("bottom left = {:?}", p.bottom_left());
 }
       
