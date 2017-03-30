3f9e7de89948c3ac6983897353af323fa375d435
Alan Morgan <thefamouseccles@gmail.com>
2017-03-29 21:31:08 -0700

    Added utils to build dominoes, triominoes, tetrominoes, and pentominoes

--- src/poly/mod.rs
+++ src/poly/mod.rs
@@ -1,3 +1,4 @@
 pub mod polyomino;
 pub mod board;
 pub mod point;
+pub mod utils;
---
+++ src/poly/utils.rs
@@ -0,0 +1,189 @@
+use poly::point::Point;
+use poly::polyomino::Polyomino;
+
+#[allow(dead_code)]
+pub fn build_monominoes() -> Vec<Polyomino> {
+    let mut r = Vec::new();
+
+    let mut p = Vec::new();
+    p.push(Point::new(0,0));
+    r.push(Polyomino::new(p));
+
+    r
+}
+
+#[allow(dead_code)]
+pub fn build_dominoes() -> Vec<Polyomino> {
+    let mut r = Vec::new();
+
+    let mut p = Vec::new();
+    p.push(Point::new(0, 0));
+    p.push(Point::new(1, 0));
+    r.push(Polyomino::new(p));
+
+    r
+}
+
+#[allow(dead_code)]
+pub fn build_triominoes() -> Vec<Polyomino> {
+    let mut r = Vec::new();
+
+    let mut p = Vec::new();
+    p.push(Point::new(0, 0));
+    p.push(Point::new(1, 0));
+    p.push(Point::new(2, 0));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0, 0));
+    p.push(Point::new(1, 0));
+    p.push(Point::new(0, 1));
+    r.push(Polyomino::new(p));
+
+    r
+}
+
+#[allow(dead_code)]
+pub fn build_tetrominoes() -> Vec<Polyomino> {
+    let mut r = Vec::new();
+    
+    let mut p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,0));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(1,0));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(0,3));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(1,1));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    r.push(Polyomino::new(p));
+
+    r
+}
+
+#[allow(dead_code)]
+pub fn build_pentominoes() -> Vec<Polyomino> {
+    let mut r = Vec::new();
+
+    let mut p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(0,3));
+    p.push(Point::new(0,4));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(0,3));
+    p.push(Point::new(1,3));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(1,2));
+    p.push(Point::new(0,3));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(1,1));
+    p.push(Point::new(2,1));
+    p.push(Point::new(0,2));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,0));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    p.push(Point::new(1,3));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    p.push(Point::new(2,2));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(1,0));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    p.push(Point::new(2,2));
+    p.push(Point::new(0,1));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(1,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    p.push(Point::new(1,2));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(0,0));
+    p.push(Point::new(1,0));
+    p.push(Point::new(2,0));
+    p.push(Point::new(0,1));
+    p.push(Point::new(0,2));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(1,0));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    p.push(Point::new(0,0));
+    p.push(Point::new(2,2));
+    r.push(Polyomino::new(p));
+
+    p = Vec::new();
+    p.push(Point::new(1,0));
+    p.push(Point::new(1,1));
+    p.push(Point::new(1,2));
+    p.push(Point::new(0,1));
+    p.push(Point::new(2,1));
+    r.push(Polyomino::new(p));
+
+    r
+}
