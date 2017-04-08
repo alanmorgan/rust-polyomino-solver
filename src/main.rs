d238b8253d5d53238f0af7c2453568a4bca46275
Alan Morgan <thefamouseccles@gmail.com>
2017-04-07 22:04:41 -0700

    Store legal candidates in bit-vec. It works!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ mod poly;
 
 use std::collections::HashSet;
 
+extern crate bit_vec;
+use bit_vec::BitVec;
+
 use poly::board::Board;
 use poly::board::board_utils;
 use poly::polyomino::Polyomino;
@@ -9,34 +12,20 @@ use poly::point::Point;
 use poly::utils;
 
 fn main() {
+    let pentominoes = build_variations(&utils::build_pentominoes());
     let mut b = Board::new(3, 20);
-    let mut candidates = get_working_set();
 
-    /*
-    let mut candidates = utils::build_pentominoes();
-    let mut rejected = Vec::new();
-
-    for p in candidates {
-        for variation in p.make_variations() {
-            // Should fit take ownership of the value instead of a ref? What to do if it fails?
-            if board_utils::fit(&mut b, variation) {
-                // Found a match. All the rejected polys are now possible candidates again
-                // candidates.extend(rejected.iter());
-                break;
-            }
-        }
-        // None of the variations worked
-        rejected.push(p);
-    }
-    */
+    fill(&mut b, &pentominoes);
 }
 
-fn get_working_set() -> Vec<HashSet<Polyomino>> {
-    build_variations(utils::build_pentominoes())
+fn fill<'a>(b : &mut Board<'a>, candidates: &'a Vec<HashSet<Polyomino>>) {
+    let mut usable_candidates = BitVec::from_elem(candidates.len(), true);
+    
+    fill_board(b, candidates, &usable_candidates);
 }
 
-fn build_variations(polys : Vec<Polyomino>) -> Vec<HashSet<Polyomino>> {
-    let mut res = Vec::new();
+fn build_variations(polys : &Vec<Polyomino>) -> Vec<HashSet<Polyomino>> {
+    let mut res = Vec::with_capacity(polys.len());
 
     for p in polys {
         res.push(p.make_variations());
@@ -45,27 +34,33 @@ fn build_variations(polys : Vec<Polyomino>) -> Vec<HashSet<Polyomino>> {
     res
 }
 
-/*
-fn fill_board<'a>(b : &mut Board<'a>, candidates: &'a Vec<HashSet<Polyomino>>) {
-    if candidates.is_empty() {
+fn fill_board<'a>(b : &mut Board<'a>, candidates: &'a Vec<HashSet<Polyomino>>, usable_candidates: &BitVec) {
+
+    if usable_candidates.none() {
         println!("{}", b);
         return;
     }
 
-    // Try each polyomino in every orientation in turn
-    for (idx, poly_variations) in candidates.iter().enumerate() {
-        // build new vector, minus this element
+    let mut uc = usable_candidates.clone();
 
-        // I can see this is going to be *horribly* inefficient
-        let l = candidates.iter().enumerate().filter(|&(i,_)| i != idx).map(|(_, e)| e.clone());
-        let remaining_candidates:Vec<HashSet<Polyomino>> = l.collect();
+    let foo = usable_candidates.iter().
+        enumerate().
+        zip(candidates.iter()).
+        filter_map(|((n, b), polys)|
+                   if b {
+                       Some((n, polys))
+                   } else {
+                       None
+                   });
 
-        for p in poly_variations {
-            if let Some(point) = board_utils::fit(b, &p) {
-                fill_board(b, &remaining_candidates);
+    for (n, polys) in foo {
+        for poly in polys {
+            if let Some(point) = board_utils::fit(b, &poly) {
+                uc.set(n, false);
+                fill_board(b, candidates, &uc);
+                uc.set(n, true);
                 b.remove_polyomino(point);
             }
         }
     }
 }
-*/
