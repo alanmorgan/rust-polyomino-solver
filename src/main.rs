1a86074a96ad77fadbf1836baa978e121de6dfd6
Alan Morgan <thefamouseccles@gmail.com>
2017-04-17 21:57:07 -0700

    Demo with new board type. Solutions correct!

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,18 @@ use poly::polyomino::polyomino_utils;
 
 fn main() {
 
-    if let Ok(pentominoes) = polyomino_utils::read_polyomino_file("data/pentomino.txt") {
-        let all_pentominoes = polyomino_utils::build_rect_variations(&pentominoes);
-        if let Ok(mut b) = Board::from_file("data/b8x8holes.txt") {
+    if let Ok(pentominoes) = polyomino_utils::read_polyomino_file("data/pentomino.poly") {
+        let all_pentominoes = polyomino_utils::build_square_variations(&pentominoes);
+        if let Ok(mut b) = Board::from_file("data/b8x8nocorners.board") {
             println!("{} solutions\n", board_utils::fill(&mut b, &all_pentominoes));
         }
+        else
+        {
+            panic!("Can't find board data/b8x8holes.board");
+        }
+    }
+    else
+    {
+        panic!("Can't find polyomino file data/pentomino.poly");
     }
 }
