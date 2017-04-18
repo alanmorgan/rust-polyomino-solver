3cfc30aa85e3e08309740446c4f7d9564bf69c22
Alan Morgan <thefamouseccles@gmail.com>
2017-04-17 21:39:27 -0700

    Read board and polys from file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 mod poly;
 
 extern crate bit_vec;
-use bit_vec::BitVec;
 
 use std::collections::HashSet;
 
 use poly::board::Board;
-use poly::board::BoardState;
 use poly::board::board_utils;
-use poly::polyomino::Polyomino;
-use poly::point::Point;
 use poly::polyomino::polyomino_utils;
 
 fn main() {
+
     if let Ok(pentominoes) = polyomino_utils::read_polyomino_file("data/pentomino.txt") {
         let all_pentominoes = polyomino_utils::build_rect_variations(&pentominoes);
-        let mut b = Board::new(20, 3);
-        println!("{} solutions\n", board_utils::fill(&mut b, &all_pentominoes));
+        if let Ok(mut b) = Board::from_file("data/b8x8holes.txt") {
+            println!("{} solutions\n", board_utils::fill(&mut b, &all_pentominoes));
+        }
     }
 }
