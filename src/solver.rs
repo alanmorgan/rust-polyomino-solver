02c8af25db2b47d585931202b8285be2e17284dc
Alan Morgan <thefamouseccles@gmail.com>
2018-05-09 21:42:50 -0700

    Remove RealSolver

--- src/solver.rs
+++ src/solver.rs
@@ -16,7 +16,8 @@ pub struct Solver<'a, 'b> {
     board: &'a mut Board<'a>,
     candidates: &'a Vec<Vec<Polyomino>>,
     region_check: Option<&'b Fn(&Board, usize)->bool>,
-    print_solutions: PrintSolutions
+    print_solutions: PrintSolutions,
+    solutions_found: u32
 }
 
 
@@ -25,7 +26,8 @@ impl<'a, 'b> Solver<'a, 'b> {
         Solver { board : b,
                  candidates: c,
                  region_check: None,
-                 print_solutions: PrintSolutions::PrintTotalOnly}
+                 print_solutions: PrintSolutions::PrintTotalOnly,
+                 solutions_found: 0}
     }
 
     #[allow(dead_code)]
@@ -40,28 +42,7 @@ impl<'a, 'b> Solver<'a, 'b> {
 
 
     pub fn solve(&mut self) -> u32 {
-        let mut real_solver = RealSolver::new(self);
-        
-        real_solver.solve()
-    }
-}
-
-// There's no way to make just some fields of a struct private, but no-one else needs to see the
-// internal bits of the solver (not that there is much). So the actual solving will be done with
-// the (not pub) RealSolver, which is built from a (pub) Solver
-struct RealSolver<'a, 'b:'a, 'c:'a> {
-    solutions_found: u32,
-    solver: &'a mut Solver<'b, 'c>
-}
-
-impl <'a, 'b, 'c> RealSolver<'a, 'b, 'c> {
-    pub fn new(s: &'a mut Solver<'b, 'c>) -> RealSolver<'a, 'b, 'c>{
-        RealSolver { solutions_found: 0,
-                     solver: s }
-    }
-
-    pub fn solve(&mut self) -> u32 {
-        let mut usable_candidates = BitVec::from_elem(self.solver.candidates.len(), true);
+        let mut usable_candidates = BitVec::from_elem(self.candidates.len(), true);
         
         self.solve_ex(&mut usable_candidates);
 
@@ -72,30 +53,30 @@ impl <'a, 'b, 'c> RealSolver<'a, 'b, 'c> {
         if usable_candidates.none() {
             self.solutions_found = self.solutions_found + 1;
 
-            match self.solver.print_solutions {
-                PrintSolutions::PrintAll => println!("Solution {}\n{}", self.solutions_found, self.solver.board),
+            match self.print_solutions {
+                PrintSolutions::PrintAll => println!("Solution {}\n{}", self.solutions_found, self.board),
                 PrintSolutions::PrintTotalOnly => (),
-                PrintSolutions::PrintEveryNth(n) => if self.solutions_found % n == 0 { println!("Solution {}\n{}", self.solutions_found, self.solver.board) }
+                PrintSolutions::PrintEveryNth(n) => if self.solutions_found % n == 0 { println!("Solution {}\n{}", self.solutions_found, self.board) }
             }
 
             return;
         }
         
-        if let Some(fit_point) = board_utils::get_first_unoccupied(&mut self.solver.board) {
-            if let Some(region_check_fn) = self.solver.region_check {
-                if !region_check_fn(self.solver.board, board_utils::get_all_adjacent(fit_point, &self.solver.board).len()) {
+        if let Some(fit_point) = board_utils::get_first_unoccupied(&mut self.board) {
+            if let Some(region_check_fn) = self.region_check {
+                if !region_check_fn(self.board, board_utils::get_all_adjacent(fit_point, &self.board).len()) {
                     return;
                 }
             }
 
             for i in 0..usable_candidates.len() {
                 if usable_candidates.get(i) == Some(true) {
-                    for poly in &self.solver.candidates[i] {
-                        if let Some(point) = board_utils::fit_at(&mut self.solver.board, &poly, fit_point) {
+                    for poly in &self.candidates[i] {
+                        if let Some(point) = board_utils::fit_at(&mut self.board, &poly, fit_point) {
                             usable_candidates.set(i, false);
                             self.solve_ex(usable_candidates);
                             usable_candidates.set(i, true);
-                            self.solver.board.remove_polyomino(point);
+                            self.board.remove_polyomino(point);
                         }
                     }
                 }
